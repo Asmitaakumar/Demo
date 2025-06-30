32d9deb661b985c503aabbcfe20777399b2fcdd1
Asmita.N <150109081+Asmitaakumar@users.noreply.github.com>
2025-06-30 13:46:02 +0530

    Update stage-db-backup.sh

--- scripts/stage-db-backup.sh
+++ scripts/stage-db-backup.sh
@@ -3,7 +3,7 @@
 #################################################################
 #  Load secrets (don't commit this file to GitHub)
 #################################################################
-source /home/rundeck/secrets/env_vars
+source /etc/rundeck/secrets/env_vars
 
 #################################################################
 #  Define constants and derived values
